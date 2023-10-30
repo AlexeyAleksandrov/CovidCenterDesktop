8edade7df2f717896fb1d9795e50bdb212af871e
AlexeyAleksandrov <chinok73@yandex.ru>
2023-10-30 21:27:14 +0300

    Начало создания коннектов

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,26 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#define PAGE_CLIENT_AUTH 0
+#define PAGE_CLIENT_REGISTRATION 1
+#define PAGE_MEMBER_AUTH 3
+#define PAGE_MEMBER_REGISTRATION 4
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // убирвем фокус на lineEdit при старте
     ui->label_logo_img->setFocusPolicy(Qt::WheelFocus);
     ui->label_logo_img->setFocus(Qt::OtherFocusReason);
+
+    // коннекты для переходов
+    connect(ui->pushButton_client_auth_move_to_registation, &QPushButton::clicked, this, [&](){
+        ui->stackedWidget_auth->setCurrentIndex(PAGE_CLIENT_REGISTRATION);
+    });     // регистрация для клиента
 }
 
 MainWindow::~MainWindow()
